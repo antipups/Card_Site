717ce29a1872c35c07970a10c15f796af71b24c2
antipups <nick.kurkurin@protonmail.com>
2020-03-29 01:33:25 +0300

    Красивые отскоки.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ animate({                               // запускаем всё
     },
     draw: function(progress)            // сама рисовалка
     {
-        elem.style.width = progress * 100 + '%';
+        elem.style.width = progress * 60 + '%';
     }
 });
 
@@ -22,45 +22,94 @@ function animate({duration, draw, timing}) {
 
         draw(progress);
 
-        if (progress < 0.6)
+        if (progress < 1)
         {
             requestAnimationFrame(animate);
         }
         else
         {
-            elem.style.width = '60%';   // костыль?)) нит)))))
             if (document.getElementsByClassName("greetings").length === 0)
             {
-                write_title();    // если приветствия ещё не было
+                write_greetings();    // если приветствия ещё не было
+            }
+            else if (!(document.getElementsByClassName("greetings")[0].classList.contains('disable_for_animations')))
+            {
+                write_farewell();
+            }
+            else
+            {
+                
             }
         }
 
     });
 }
 
-function write_title()
+function write_greetings()
 {
     let work_div = document.getElementById('elem');
     let new_title = document.createElement('p');
     new_title.textContent = "Здравствуйте";
     new_title.classList.add('greetings');
-    new_title.style.fontSize = 0.5 * 200 + "px";
-    new_title.style.color = 'rgba(0,0,0,0)';
+    new_title.style.fontSize = "750%";
+    new_title.style.bottom = '100%'
     work_div.appendChild(new_title);
+
+    // для отскоков с другой стороны (1-)
+    function makeEaseOut(timing) {
+        return function(timeFraction) {
+            return 1 - timing(1 - timeFraction);
+        }
+    }
+
+    // для просто отскоков
+    function bounce(timeFraction) {
+        for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+            if (timeFraction >= (7 - 4 * a) / 11) {
+                return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+            }
+        }
+    }
+
+    let bounceEaseOut = makeEaseOut(bounce);
 
     animate
     (
         {
-            duration: 3000,                     // длительность всей анимации
-            timing: function(timeFraction)      // время наростания (как энергично она будет происходить)
-            {
-                return Math.pow(timeFraction, 5);
-            },
+            duration: 1000,                     // длительность всей анимации
+            timing: bounceEaseOut,      // время наростания (как энергично она будет происходить)
             draw:function(progress)            // сама рисовалка
             {
-                new_title.style.color = 'rgba(0,0,0,' + +(progress * 100) + ')';
+                // new_title.style.color = 'rgba(0,0,0,' + progress + ')';
+                new_title.style.bottom = (1 - progress) * 100 + "%";
+                console.log((1 - progress) * 100)
             }
         }
     );
 
+}
+
+
+function write_farewell()
+{
+    let work_div = document.getElementById('elem');
+    let greetings = document.getElementsByTagName('p')[0];
+    // animate
+    // (
+    //     {
+    //         duration: 3000,                     // длительность всей анимации
+    //         timing: function(timeFraction)      // время наростания (как энергично она будет происходить)
+    //         {
+    //             return Math.pow(timeFraction, 5);
+    //         },
+    //         draw:function(progress)            // сама рисовалка
+    //         {
+    //             if (1 - progress * 1.8 <= 0)
+    //             {
+    //                 greetings.classList.add("disable_for_animations");
+    //             }
+    //             greetings.style.color = 'rgba(0,0,0,' + +(1 - progress * 1.8) + ')';
+    //         }
+    //     }
+    // );
 }
